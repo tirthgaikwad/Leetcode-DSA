f20a05700ffe62e8822d4c42a44e9b608b80e337
Gaikwad Tirth Yogesh <tirthgaikwad30@gmail.com>
2026-07-29 22:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (78.12%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isAlphaNum(char ch) {
+        return (ch >= '0' && ch <= '9') ||
+               (tolower(ch) >= 'a' && tolower(ch) <= 'z');
+    }
+
+    bool isPalindrome(string s) {
+        int st = 0;
+        int end = s.length() - 1;
+
+        while (st < end) {
+
+            if (!isAlphaNum(s[st])) {
+                st++;
+                continue;
+            }
+
+            if (!isAlphaNum(s[end])) {
+                end--;
+                continue;
+            }
+
+            if (tolower(s[st]) != tolower(s[end])) {
+                return false;
+            }
+
+            st++;
+            end--;
+        }
+
+        return true;
+    }
+};
